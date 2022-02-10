aff7a58165e7998882dc887b6d73229eac218039
MetalJames <60982423+MetalJames@users.noreply.github.com>
2022-02-10 14:00:31 +0200

    Create index.js
    To make it clean in App.js export all components from 1 single file index.js

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as NavBar } from './NavBar/NavBar';
+export { default as Products } from './Products/Products';
+export { default as Cart } from './Cart/Cart';
+export { default as Checkout } from './CheckoutForm/Checkout/Checkout';
+export { default as Footer } from './Footer/Footer';
+export { default as MoreInfo } from './Products/Product/ProductMore/ProductMore';
+export { default as Gallery } from './Products/Product/ProductMore/Gallery';
